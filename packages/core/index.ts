95a2d7361ad37ce821cd3bc2a125305a5cb78897
GhostFace <1789063411@qq.com>
2024-08-15 21:02:16 +0800

    commit

--- packages/core/index.ts
+++ packages/core/index.ts
@@ -1,5 +1,6 @@
 import { makeInstaller } from '@zz_components_library/utils'
 import components from './components'
+import '@zz_components_library/theme/index.css'
 
 //使用者使用时可以用vue的plugin来使用，并将组件挂载到vue实例上
 const installer = makeInstaller(components)
